648e8090faa67e621ecb95b7aaf889c7c6d10ecd
Michael Bädorf <tailoringexpert@michael-baedorf.de>
2026-01-26 14:30:17 +0100

    [WIP] reqif implementation

--- tailoringexpert-reqif/src/main/java/eu/tailoringexpert/reqif/ReqIFContentSerializer.java
+++ tailoringexpert-reqif/src/main/java/eu/tailoringexpert/reqif/ReqIFContentSerializer.java
@@ -1,15 +1,9 @@
 package eu.tailoringexpert.reqif;
 
-import eu.tailoringexpert.domain.AttributeDefinition;
-import eu.tailoringexpert.domain.AttributeDefinitionBoolean;
-import eu.tailoringexpert.domain.AttributeDefinitionString;
 import eu.tailoringexpert.domain.DatatypeDefinition;
-import eu.tailoringexpert.domain.Identifiable;
 import eu.tailoringexpert.domain.ReqIFContent;
 import eu.tailoringexpert.domain.SpecObject;
-import eu.tailoringexpert.domain.SpecObjectType;
 import eu.tailoringexpert.domain.SpecType;
-import eu.tailoringexpert.domain.SpecificationType;
 import tools.jackson.core.JacksonException;
 import tools.jackson.core.JsonGenerator;
 import tools.jackson.databind.SerializationContext;
@@ -18,19 +12,14 @@ import tools.jackson.dataformat.xml.ser.ToXmlGenerator;
 
 import javax.xml.namespace.QName;
 import java.util.Collection;
-import java.util.Map;
 import java.util.function.BiConsumer;
-
-import static java.util.Map.entry;
 
 
 public class ReqIFContentSerializer extends StdSerializer<ReqIFContent> {
 
-    private BiConsumer<Identifiable, ToXmlGenerator> identifiable = new IdentifiableConsumer();
     private BiConsumer<Collection<DatatypeDefinition>, ToXmlGenerator> datatypes = new DatatypeDefinitionsConsumer();
     private BiConsumer<Collection<SpecType>, ToXmlGenerator> specTypes = new SpecTypesConsumer();
     private BiConsumer<Collection<SpecObject>, ToXmlGenerator> specObjects = new SpecObjectsConsumer();
-
 
     public ReqIFContentSerializer() {
         super(ReqIFContent.class);
