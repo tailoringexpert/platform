7fa63d6bb4928eb58adc27d0262a2c2939b713ba
Michael Bädorf <tailoringexpert@michael-baedorf.de>
2026-01-28 13:06:39 +0100

    [WIP] reqif implementation

--- tailoringexpert-reqif/src/main/java/eu/tailoringexpert/reqif/SpecObjectsConsumer.java
+++ tailoringexpert-reqif/src/main/java/eu/tailoringexpert/reqif/SpecObjectsConsumer.java
@@ -9,17 +9,23 @@ import java.util.function.BiConsumer;
 
 public class SpecObjectsConsumer implements BiConsumer<Collection<SpecObject>, ToXmlGenerator> {
 
+    private static final QName QNAME_SPECOBJECTS = new QName("SPEC-OBJECTS");
+
     private BiConsumer<SpecObject, ToXmlGenerator> specObject = new SpecObjectConsumer();
 
     @Override
     public void accept(Collection<SpecObject> specObjects, ToXmlGenerator generator) {
-        QName name = new QName("SPEC-OBJECTS");
-        generator.startWrappedValue(name, name);
-        generator.setNextIsAttribute(false);
+//        QName name = new QName("SPEC-OBJECTS");
+//        generator.startWrappedValue(name, name);
+//        generator.setNextIsAttribute(false);
+//
+//        specObjects
+//            .forEach(value -> specObject.accept(value, generator));
+//
+//        generator.finishWrappedValue(name, name);
 
-        specObjects
-            .forEach(value -> specObject.accept(value, generator));
-
-        generator.finishWrappedValue(name, name);
+        generator.startWrappedValue(QNAME_SPECOBJECTS, QNAME_SPECOBJECTS);
+        specObjects.forEach(value -> specObject.accept(value, generator));
+        generator.finishWrappedValue(QNAME_SPECOBJECTS, QNAME_SPECOBJECTS);
     }
 }
