eb4337c6c3183c70cbaeffd882ec139d933dce2c
Michael Bädorf <tailoringexpert@m.familiebaedorf.de>
2026-06-29 10:01:57 +0000

    i dont know

--- tailoringexpert-openhtmltopdf/src/test/java/eu/tailoringexpert/tailoring/ReequirementDiffTest.java
+++ tailoringexpert-openhtmltopdf/src/test/java/eu/tailoringexpert/tailoring/ReequirementDiffTest.java
@@ -25,189 +25,158 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class ReequirementDiffTest {
 
-    DiffRowGenerator generator;
+        DiffRowGenerator generator;
 
-    @BeforeEach
-    void BeforeEach() {
-        this.generator = DiffRowGenerator.create()
-                .showInlineDiffs(true)
-                .inlineDiffByWord(true)
-                .oldTag(f -> "~")
-                .newTag(f -> "**")
-                .build();
-    }
-
-    @Test
-    void doit() {
-        // arrange
-        List<DiffRow> rows = generator.generateDiffRows(
-                Arrays.asList("This is a test sentence.", "This is the second line.", "And here is the finish."),
-                Arrays.asList("This is a test for diffutils.", "This is the second line."));
-
-        // act
-
-        // assert
-        log.debug("|original|new|");
-        log.debug("|--------|---|");
-        for (DiffRow row : rows) {
-            log.debug("|" + row.getOldLine() + "|" + row.getNewLine() + "|");
+        @BeforeEach
+        void BeforeEach() {
+                this.generator = DiffRowGenerator.create()
+                                .showInlineDiffs(true)
+                                .inlineDiffByWord(true)
+                                .oldTag(f -> "~")
+                                .newTag(f -> "**")
+                                .build();
         }
 
-    }
+        @Test
+        void doit() {
+                // arrange
+                List<DiffRow> rows = generator.generateDiffRows(
+                                Arrays.asList("This is a test sentence.", "This is the second line.",
+                                                "And here is the finish."),
+                                Arrays.asList("This is a test for diffutils.", "This is the second line."));
 
-    @Test
-    void doit2() {
-        // arrange
-        Chapter<TailoringRequirement> base = Chapter.<TailoringRequirement>builder()
-                .number("1")
-                .requirements(List.of(
-                        TailoringRequirement.builder()
-                                .position("a")
-                                .selected(Boolean.FALSE)
-                                .text("Sample 1")
-                                .build(),
-                        TailoringRequirement.builder()
-                                .position("b")
-                                .selected(Boolean.FALSE)
-                                .text("Sample 2")
-                                .build()))
-                .chapters(List.of(
-                        Chapter.<TailoringRequirement>builder()
-                                .number("1.1")
-                                .requirements(List.of(
-                                        TailoringRequirement.builder()
-                                                .position("a")
-                                                .selected(Boolean.FALSE)
-                                                .text("Sample 1.1")
-                                                .build()))
-                                .build()))
-                .build();
+                // act
 
-        Chapter<TailoringRequirement> compare = Chapter.<TailoringRequirement>builder()
-                .number("1")
-                .requirements(List.of(
-                        TailoringRequirement.builder()
-                                .position("a")
-                                .selected(Boolean.TRUE)
-                                .text("Sample 3")
-                                .build(),
-                        TailoringRequirement.builder()
-                                .position("b")
-                                .selected(Boolean.TRUE)
-                                .text("Sample 4")
-                                .build()))
-                .chapters(List.of(
-                        Chapter.<TailoringRequirement>builder()
-                                .number("1.1")
-                                .requirements(List.of(
-                                        TailoringRequirement.builder()
-                                                .position("a")
-                                                .selected(Boolean.FALSE)
-                                                .text("Sample 1.1 hanged")
-                                                .build()))
-                                .build()))
-                .build();
+                // assert
+                log.debug("|original|new|");
+                log.debug("|--------|---|");
+                for (DiffRow row : rows) {
+                        log.debug("|" + row.getOldLine() + "|" + row.getNewLine() + "|");
+                }
 
-        Map<String, List<TailoringRequirementDiff>> actual = new HashMap<>();
-
-        // act
-        accept(base, compare, actual);
-
-        // assert
-        assertThat(actual)
-                .isNotEmpty()
-                .hasSize(2);
-        assertThat(actual.get("1"))
-                .hasSize(2);
-
-    }
-
-    private void accept(Chapter<TailoringRequirement> base,
-            Chapter<TailoringRequirement> compare,
-            Map<String, List<TailoringRequirementDiff>> diffs) {
-
-        compare.getRequirements().forEach(requirement -> accept(
-                requirement,
-                base.getRequirement(requirement.getPosition()).get(),
-                diffs.computeIfAbsent(base.getNumber(), key -> new ArrayList<>())));
-
-        Optional.ofNullable(compare.getChapters()).ifPresent(subChapters -> subChapters.forEach(subChapter -> accept(
-                subChapter,
-                base.getChapter(subChapter.getNumber()),
-                diffs)));
-
-        // () -> revised.getRequirements().forEach(requirement ->
-        // accept(
-        // requirement,
-        // empty(),
-        // replacements
-        // )
-        // )
-        // );
-        // base.ifPresentOrElse(baseChapter ->
-        // revised.getRequirements().forEach(requirement ->
-        // accept(
-        // requirement,
-        // baseChapter.getRequirement(requirement.getPosition())
-        // )
-        // ),
-        // () -> revised.getRequirements().forEach(requirement ->
-        // accept(
-        // requirement,
-        // empty()
-        // )
-        // )
-        // );
-
-        // ofNullable(revised.getChapters()).ifPresent(subChapters ->
-        // subChapters.forEach(subChapter ->
-        // accept(
-        // subChapter,
-        // base.map(baseSubChapter ->
-        // baseSubChapter.getChapter(subChapter.getNumber())),
-        // replacements
-        // )
-        // )
-        // );
-    }
-
-    void accept(TailoringRequirement base, TailoringRequirement compare, List<TailoringRequirementDiff> diff) {
-        List<DiffRow> diffs = generator.generateDiffRows(
-                List.of(base.getText()),
-                List.of(compare.getText()));
-
-        if (diffs.isEmpty() || (!hasTextDiff(diffs) && !hasSelectionDiff(base, compare))) {
-            return; // Optional.empty();
         }
 
-        diff.add(TailoringRequirementDiff.builder()
-                .base(TailoringRequirement.builder()
-                        .position(base.getPosition())
-                        .text(diffs.get(0).getOldLine())
-                        .selected(base.getSelected())
-                        .build())
-                .other(TailoringRequirement.builder()
-                        .position(compare.getPosition())
-                        .text(diffs.get(0).getNewLine())
-                        .selected(compare.getSelected())
-                        .build())
-                .build());
+        @Test
+        void doit2() {
+                // arrange
+                Chapter<TailoringRequirement> base = Chapter.<TailoringRequirement>builder()
+                                .number("1")
+                                .requirements(List.of(
+                                                TailoringRequirement.builder()
+                                                                .position("a")
+                                                                .selected(Boolean.FALSE)
+                                                                .text("Sample 1")
+                                                                .build(),
+                                                TailoringRequirement.builder()
+                                                                .position("b")
+                                                                .selected(Boolean.FALSE)
+                                                                .text("Sample 2")
+                                                                .build()))
+                                .chapters(List.of(
+                                                Chapter.<TailoringRequirement>builder()
+                                                                .number("1.1")
+                                                                .requirements(List.of(
+                                                                                TailoringRequirement.builder()
+                                                                                                .position("a")
+                                                                                                .selected(Boolean.FALSE)
+                                                                                                .text("Sample 1.1")
+                                                                                                .build()))
+                                                                .build()))
+                                .build();
 
-    }
+                Chapter<TailoringRequirement> compare = Chapter.<TailoringRequirement>builder()
+                                .number("1")
+                                .requirements(List.of(
+                                                TailoringRequirement.builder()
+                                                                .position("a")
+                                                                .selected(Boolean.TRUE)
+                                                                .text("Sample 3")
+                                                                .build(),
+                                                TailoringRequirement.builder()
+                                                                .position("b")
+                                                                .selected(Boolean.TRUE)
+                                                                .text("Sample 4")
+                                                                .build()))
+                                .chapters(List.of(
+                                                Chapter.<TailoringRequirement>builder()
+                                                                .number("1.1")
+                                                                .requirements(List.of(
+                                                                                TailoringRequirement.builder()
+                                                                                                .position("a")
+                                                                                                .selected(Boolean.FALSE)
+                                                                                                .text("Sample 1.1 hanged")
+                                                                                                .build()))
+                                                                .build()))
+                                .build();
 
-    boolean hasSelectionDiff(TailoringRequirement base, TailoringRequirement compare) {
-        return !base.getSelected().equals(compare.getSelected());
-    }
+                Map<String, List<TailoringRequirementDiff>> actual = new HashMap<>();
 
-    boolean hasTextDiff(List<DiffRow> diffs) {
-        return !diffs.isEmpty() || !diffs.getFirst().getTag().equals(Tag.EQUAL);
-    }
+                // act
+                accept(base, compare, actual);
 
-    @Builder
-    @Value
-    public static class TailoringRequirementDiff {
-        TailoringRequirement base;
-        TailoringRequirement other;
+                // assert
+                assertThat(actual)
+                                .isNotEmpty()
+                                .hasSize(2);
+                assertThat(actual.get("1"))
+                                .hasSize(2);
 
-    }
+        }
+
+        private void accept(Chapter<TailoringRequirement> base,
+                        Chapter<TailoringRequirement> compare,
+                        Map<String, List<TailoringRequirementDiff>> diffs) {
+
+                compare.getRequirements().forEach(requirement -> accept(
+                                requirement,
+                                base.getRequirement(requirement.getPosition()).get(),
+                                diffs.computeIfAbsent(base.getNumber(), key -> new ArrayList<>())));
+
+                Optional.ofNullable(compare.getChapters())
+                                .ifPresent(subChapters -> subChapters.forEach(subChapter -> accept(
+                                                subChapter,
+                                                base.getChapter(subChapter.getNumber()),
+                                                diffs)));
+
+        }
+
+        void accept(TailoringRequirement base, TailoringRequirement compare, List<TailoringRequirementDiff> diff) {
+                List<DiffRow> diffs = generator.generateDiffRows(
+                                List.of(base.getText()),
+                                List.of(compare.getText()));
+
+                if (diffs.isEmpty() || (!hasTextDiff(diffs) && !hasSelectionDiff(base, compare))) {
+                        return; // Optional.empty();
+                }
+
+                diff.add(TailoringRequirementDiff.builder()
+                                .base(TailoringRequirement.builder()
+                                                .position(base.getPosition())
+                                                .text(diffs.get(0).getOldLine())
+                                                .selected(base.getSelected())
+                                                .build())
+                                .other(TailoringRequirement.builder()
+                                                .position(compare.getPosition())
+                                                .text(diffs.get(0).getNewLine())
+                                                .selected(compare.getSelected())
+                                                .build())
+                                .build());
+
+        }
+
+        boolean hasSelectionDiff(TailoringRequirement base, TailoringRequirement compare) {
+                return !base.getSelected().equals(compare.getSelected());
+        }
+
+        boolean hasTextDiff(List<DiffRow> diffs) {
+                return !diffs.isEmpty() || !diffs.getFirst().getTag().equals(Tag.EQUAL);
+        }
+
+        @Builder
+        @Value
+        public static class TailoringRequirementDiff {
+                TailoringRequirement base;
+                TailoringRequirement other;
+
+        }
 }
