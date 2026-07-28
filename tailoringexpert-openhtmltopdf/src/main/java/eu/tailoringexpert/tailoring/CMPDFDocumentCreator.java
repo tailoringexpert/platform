92f6e596cf9665e840c8e1f1fa138c695ecd8708
Michael Bädorf <tailoringexpert@m.familiebaedorf.de>
2026-07-28 05:32:27 +0000

    removed uc tailoring

--- tailoringexpert-openhtmltopdf/src/main/java/eu/tailoringexpert/tailoring/CMPDFDocumentCreator.java
+++ tailoringexpert-openhtmltopdf/src/main/java/eu/tailoringexpert/tailoring/CMPDFDocumentCreator.java
@@ -52,6 +52,7 @@ import lombok.extern.log4j.Log4j2;
  */
 @RequiredArgsConstructor
 @Log4j2
+@SuppressWarnings("common-sonar:DuplicatedBlocks")
 public class CMPDFDocumentCreator implements DocumentCreator {
 
     @NonNull
